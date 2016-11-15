b9b9e1d47676dda28d0551edf5e168733c1b421c
Amritpal17 <angybird.17@gmail.com>
2016-11-15 11:05:01 +0530

    Create info.cpp

---
+++ info.cpp
@@ -0,0 +1,57 @@
+// For Reference
+/*
+
+Records:
+	1. Employee
+		-emp_code
+		-emp_name
+		-address
+		-phone_no
+		-date_of_joining
+		-designation
+		-salary
+			+ basic_salary
+			+ hra
+			+ convence
+			+ LTA
+			+ Medical
+			+ PF
+Details:
+	1. Product
+		-product_code
+		-product_name
+		-product_price
+	2. Recipt
+		-recipt_no
+		-date
+		-sale_order_no
+		-amount
+		-recipt_no
+	3. Payment -----DONE
+		-vendor_code
+		-vendor_name
+		-address
+		-phone_no
+	4. Vendor  ---- SAME AS THE PAYMENT
+		-vendor_code
+		-vendor_name
+		-address
+		-phone_no
+Forms:
+	1. Purchase
+		-purchase_no
+		-purchase_date
+		-supplier_code
+		-product_code
+		-product_cost
+		-quantity
+		-amount
+	2. Sale
+		-sales_no
+		-sales_date
+		-costumer_info
+		-product_info
+		-sale_price
+		-quantity
+		-amount
+*/
